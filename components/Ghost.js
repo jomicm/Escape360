fb40bf0f0493afe84cf349bea84d4dd6173d260c
Juan Carrera <juankarrera@gmail.com>
2019-11-26 13:35:03 -0500

    add chatbox to ghost in the correct position

--- components/Ghost.js
+++ components/Ghost.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import {
   View,
   Image,
+  Text,
   VrButton,
   asset,
-  Stylesheet
+  StyleSheet
 } from 'react-360';
 import { dataStore, getPuzzleAnswers, componentsMgmt } from '../index';
 
@@ -13,6 +14,7 @@ export default class Ghost extends Component {
 
   state = {
     show: true,
+    isClicked: true,
     
   }
 
@@ -23,10 +25,27 @@ export default class Ghost extends Component {
     }
   }
 
+  handleOnClick = () => {
+    console.log('BOOOOOOOO from Casper the ghost!!');
+  }
+
+  ghostMessage = () => {
+    return 'BOOOOOOOO from Casper the ghost!!';
+  }
+
   render = () => {
     return (
       <View>
         {this.state.show && <View style={styles.container}>
+          <View style={styles.children}>
+            <VrButton onClick={this.handleOnClick}><View style={styles.button}></View></VrButton>
+          </View>
+          <View style={[styles.children, {flex: 2}]}>
+            {this.state.isClicked && <View>
+              <Image style={styles.image} source={asset('chatbox.png')}/>
+              <Text style={styles.text}>{this.ghostMessage}</Text>
+            </View>}
+          </View>
         </View>}
       </View>
     )
@@ -37,8 +56,29 @@ export default class Ghost extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'rgba(59, 198, 140, 0.5)',
-    width: 400,
-    height: 320
+    // backgroundColor: 'rgba(59, 198, 140, 0.5)',
+    width: 250,
+    height: 350,
+    flex: 1,
+    flexDirection: 'row',
+  },
+  children: {
+    // borderColor: "#639dda",
+    // borderWidth: 2,
+    flex: 1.5,
+
+  },
+  button: {
+    height: 350,
+    borderColor: "red",
+    borderWidth: 2,
+  },
+  image: {
+    width: 125,
+    height: 80,
+    transform: [{scaleX: -1}],
+  },
+  text: {
+    fontSize: 50
   }
 })
--- src/helpers/globalComponents.js
+++ src/helpers/globalComponents.js
@@ -18,7 +18,7 @@ module.exports = (width, height) => {
       { name: 'myGoBackDoorSurface', root:'GoBackDoor', props: { component: 'goBackDoor', room: 'bedroom' }, size: { width: 300, height: 180 }, angle: [-Math.PI + 0.8, -0.25, 0], radius: 0 },
       { name: 'mySimonFixedSurface', root: 'SimonFixed', size: { width: 200, height: 200 }, angle: [-0.452, 0.035, 0], radius: 0},
       { name: 'mySimonDynamicSurface', root: 'SimonDynamic', size: { width: 600, height: 600 }, angle: [-0.39 - Math.PI, 0.12, 0], radius: 0},
-      { name: 'myGhostSurface', root: 'Ghost', size: { width: 200, height: 400 }, angle: [-0.56, 0, 0], radius: 0},
+      { name: 'myGhostSurface', root: 'Ghost', size: { width: 250, height: 350 }, angle: [-0.78, -0.05, 0], radius: 5},
     ]
   };
 };
